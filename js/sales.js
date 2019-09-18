5678a1efeac2c3c738ebc74ba52aff1d5899c64c
huangrkul <huangrkul@gmail.com>
2019-09-18 16:04:26 -0700

    double-check

--- js/sales.js
+++ js/sales.js
@@ -1,32 +1,3 @@
-//lab8
-//event.preventDefault(); for preventing refreshing or taking to new page.  ideally go into submit for testing.
-
-//target form id in order to grab all the input values within it.  Input value can be grabbed by id: event.target.inputID.value
-
-//event.target.inputIDname.value //placed in function that handles event listener event. This outputs the value.
-
-//set event target value to null at the end of handler function to clear it.
-//text field need to be grabbed as id of textarea + .value (to grab the actual value)
-
-//+ sign before the parameter will evaluate the argument as interger.
-
-// Today you will be adding a form to your existing cookie stand project so that you can add new locations to the table by simply inputting their information with the form.
-
-// Create a new HTML form to accept the information for a new cookie stand. Be sure to utulize the <fieldset> tag to help you style it.
-
-// Upon sumbission of the HTML form, create an event handler that creates a new instance of a cookie stand that appends to the table upon form submission.
-
-// Use the constructor function as your guide to determine what input fields your form needs (hint: also consider what is passed in when creating instances!).
-
-// If not complete from lab 7, write a stand-alone function to generate a footer row which will display the total number of cookies sold per hour for all locations. When a new store is added using your form, the totals in the footer row should update to include these new sales numbers.
-
-// Anywhere you have repeated chunks of code, apply some DRY principles. Generally, once some chunk of code is appearing for a 3rd time or so, that’s when you want to consider refactoring.
-
-// Validate your html through HTML5 validation.
-
-// Confirm that your code is following the single responsibility rule. Each function should only do one thing, with the capability to break it out further as needed.
-
-
 //anchor variables
 var mainTable = document.getElementById('contentTable');
 var storeForm = document.getElementById('addStoreForm');
